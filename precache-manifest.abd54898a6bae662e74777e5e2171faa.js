b1e03aa1fb50a448b7abd1e23645b141eb54dd84
Tigran <tigran.yengibaryan2004@gmail.com>
2020-08-31 17:04:28 +0400

    Changing shorcuts to files

---
+++ precache-manifest.abd54898a6bae662e74777e5e2171faa.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f762f93ae4e1be951cbe18e9a342e2e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff562274361cea7db6f8",
+    "url": "/static/css/2.cfc6a618.chunk.css"
+  },
+  {
+    "revision": "e5612a087cb3f98e2eb0",
+    "url": "/static/css/3.5fbb6285.chunk.css"
+  },
+  {
+    "revision": "3c55a01d66e70e37c78e",
+    "url": "/static/css/4.928cf868.chunk.css"
+  },
+  {
+    "revision": "dd8a6a8181fbfd7be9ea",
+    "url": "/static/css/5.f0ff3772.chunk.css"
+  },
+  {
+    "revision": "28766c023907f33bee21",
+    "url": "/static/css/6.d87fac09.chunk.css"
+  },
+  {
+    "revision": "aa2345f8e7ae73d3adc1",
+    "url": "/static/css/7.e673f714.chunk.css"
+  },
+  {
+    "revision": "995d11f691ee0f3c67fb",
+    "url": "/static/css/main.f6c60dce.chunk.css"
+  },
+  {
+    "revision": "ff562274361cea7db6f8",
+    "url": "/static/js/2.c6629385.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.c6629385.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5612a087cb3f98e2eb0",
+    "url": "/static/js/3.1f5edf48.chunk.js"
+  },
+  {
+    "revision": "3c55a01d66e70e37c78e",
+    "url": "/static/js/4.c4d3ab4a.chunk.js"
+  },
+  {
+    "revision": "dd8a6a8181fbfd7be9ea",
+    "url": "/static/js/5.ebc1a606.chunk.js"
+  },
+  {
+    "revision": "28766c023907f33bee21",
+    "url": "/static/js/6.8c81de5a.chunk.js"
+  },
+  {
+    "revision": "aa2345f8e7ae73d3adc1",
+    "url": "/static/js/7.5e58c77c.chunk.js"
+  },
+  {
+    "revision": "0d8e1328c07191a179d7",
+    "url": "/static/js/8.a60a21e7.chunk.js"
+  },
+  {
+    "revision": "995d11f691ee0f3c67fb",
+    "url": "/static/js/main.7d18b8f6.chunk.js"
+  },
+  {
+    "revision": "ceb295c1bb597e2a365a",
+    "url": "/static/js/runtime-main.6fb4229b.js"
+  },
+  {
+    "revision": "f4b306eed95aa7d274840533be635532",
+    "url": "/static/media/Georgia-Regular-font.f4b306ee.ttf"
+  },
+  {
+    "revision": "9a64183b72701765451154a39c91a323",
+    "url": "/static/media/Georgia.9a64183b.ttf"
+  },
+  {
+    "revision": "e07df86cef2e721115583d61d1fb68a6",
+    "url": "/static/media/Roboto-Bold.e07df86c.ttf"
+  },
+  {
+    "revision": "88823c2015ffd5fa89d567e17297a137",
+    "url": "/static/media/Roboto-Light.88823c20.ttf"
+  },
+  {
+    "revision": "11eabca2251325cfc5589c9c6fb57b46",
+    "url": "/static/media/Roboto-Regular.11eabca2.ttf"
+  },
+  {
+    "revision": "47fb78fdad2126d020557064a99eafb3",
+    "url": "/static/media/driving-license-back.47fb78fd.png"
+  },
+  {
+    "revision": "dd5c457a141efaf1d2542e52bab98a4e",
+    "url": "/static/media/driving-license-front.dd5c457a.png"
+  },
+  {
+    "revision": "5c58d0e1d2ec48e766846af99eaeb6f8",
+    "url": "/static/media/flags.5c58d0e1.png"
+  },
+  {
+    "revision": "27670c535824abb6ae7263d60fdc8a86",
+    "url": "/static/media/flags@2x.27670c53.png"
+  },
+  {
+    "revision": "4a1b5e48d5e2b999204cbf90926b0db3",
+    "url": "/static/media/location.4a1b5e48.svg"
+  },
+  {
+    "revision": "a337c18b26e2215d201320e8cbb67da4",
+    "url": "/static/media/loginPhoto.a337c18b.png"
+  },
+  {
+    "revision": "874e5c47d5b606d979eae7873e320e09",
+    "url": "/static/media/logo.874e5c47.svg"
+  }
+]);
